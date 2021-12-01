d56665bf08b1d8bb9371fd47755af689d6ca1d32
santiago-jv <santiagoolayojv@gmail.com>
2021-12-01 02:32:39 -0500

    production problem solved

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import cors from 'cors';
 import indexRouter from './routes/indexRouter';
 import './database/connection';
 import dotenv from 'dotenv'
-import morgan from 'morgan';
 import errorHandlerMiddleware from './middlewares/errorHandler';
 
 //configuration
 dotenv.config();
 app.use(cors())
-app.use(morgan('dev'))
+
 app.use(json({limit:1024}))
 app.set('port', process.env.PORT || 8080)
 
